334ad3ed3df8bef28a1ca3188c34fa8d08c5bb8c
maikolec <maikolechenique@gmail.com>
2022-03-14 17:59:06 -0400

    Actualizacion de Interfaz Control

--- src/app/views/control/buzon/buzon.component.ts
+++ src/app/views/control/buzon/buzon.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @Component({
   selector: 'app-buzon',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuzonComponent implements OnInit {
 
-  constructor() { }
+
+  masterSelected:boolean;
+  checklist:any;
+  checkedList:any;
+
+  constructor(){
+    this.masterSelected = false;
+    this.checklist = [
+      {id:1,value:'CG-00001  ',isSelected:false},
+      // {id:2,value:'Caden Kunze',isSelected:false},
+      // {id:3,value:'Ms. Hortense Zulauf',isSelected:true},
+      // {id:4,value:'Grady Reichert',isSelected:true},
+      // {id:5,value:'Dejon Olson',isSelected:true},
+      // {id:6,value:'Jamir Pfannerstill',isSelected:false},
+      // {id:7,value:'Aracely Renner DVM',isSelected:false},
+      // {id:8,value:'Genoveva Luettgen',isSelected:false}
+    ];
+    this.getCheckedItemList();
+}
+
+ // The master checkbox will check/ uncheck all items
+ checkUncheckAll() {
+  for (var i = 0; i < this.checklist.length; i++) {
+    this.checklist[i].isSelected = this.masterSelected;
+  }
+  this.getCheckedItemList();
+}
+
+// Check All Checkbox Checked
+isAllSelected() {
+  this.masterSelected = this.checklist.every(function(item:any) {
+      return item.isSelected == true;
+    })
+  this.getCheckedItemList();
+}
+
+// Get List of Checked Items
+getCheckedItemList(){
+  this.checkedList = [];
+  for (var i = 0; i < this.checklist.length; i++) {
+    if(this.checklist[i].isSelected)
+    this.checkedList.push(this.checklist[i]);
+  }
+  this.checkedList = JSON.stringify(this.checkedList);
+}
 
   ngOnInit(): void {
   }
 
+
 }
+
+
