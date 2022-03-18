b4762668c5f3db7e77f6887686654576fa22d39b
maikolec <maikolechenique@gmail.com>
2022-03-18 18:57:14 -0400

    Actualización de Interfaces COntrol

--- src/app/views/control/registrar/registrar.component.ts
+++ src/app/views/control/registrar/registrar.component.ts
@@ -51,10 +51,26 @@ export class RegistrarComponent implements OnInit {
   // MatPaginator Output
   pageEvent: PageEvent;
 
+  masterSelected:boolean;
+  checklist:any;
+  checkedList:any;
+
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.masterSelected = false;
+    this.checklist = [
+      {id:1,value:'CG-00001  ',isSelected:false},
+      // {id:2,value:'Caden Kunze',isSelected:false},
+      // {id:3,value:'Ms. Hortense Zulauf',isSelected:true},
+      // {id:4,value:'Grady Reichert',isSelected:true},
+      // {id:5,value:'Dejon Olson',isSelected:true},
+      // {id:6,value:'Jamir Pfannerstill',isSelected:false},
+      // {id:7,value:'Aracely Renner DVM',isSelected:false},
+      // {id:8,value:'Genoveva Luettgen',isSelected:false}
+    ];
+    this.getCheckedItemList();
     
   }
 
@@ -111,6 +127,32 @@ export class RegistrarComponent implements OnInit {
 
 
   }
+
+ // The master checkbox will check/ uncheck all items
+ checkUncheckAll() {
+  for (var i = 0; i < this.checklist.length; i++) {
+    this.checklist[i].isSelected = this.masterSelected;
+  }
+  this.getCheckedItemList();
+}
+
+// Check All Checkbox Checked
+isAllSelected() {
+  this.masterSelected = this.checklist.every(function(item:any) {
+      return item.isSelected == true;
+    })
+  this.getCheckedItemList();
+}
+
+// Get List of Checked Items
+getCheckedItemList(){
+  this.checkedList = [];
+  for (var i = 0; i < this.checklist.length; i++) {
+    if(this.checklist[i].isSelected)
+    this.checkedList.push(this.checklist[i]);
+  }
+  this.checkedList = JSON.stringify(this.checkedList);
+}
 
 
 }
