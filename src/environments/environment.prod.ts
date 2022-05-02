ffebee1d38ac56ae971d4f0a9a3d854abf1d1263
Carlos Peña <gesaodin@gmail.com>
2022-05-02 13:18:14 -0400

    Version MPPD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   ID : 'ID-001',
-  Url: 'https://localhost',
+  Url: 'https://10.120.0.58',
   API: '/devel/api/',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4'
 };
