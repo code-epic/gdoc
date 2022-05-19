f39ab8c88684f37e52e499aa52a50f042b4f1e5b
Carlos Peña <gesaodin@gmail.com>
2022-05-19 17:52:56 -0400

    Verificando Buzon de Msj

--- src/app/views/control/buzon/buzon.component.ts
+++ src/app/views/control/buzon/buzon.component.ts
@@ -1,7 +1,6 @@
-import { ClassGetter, THIS_EXPR, ThrowStmt } from '@angular/compiler/src/output/output_ast';
+
 import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
-
 import { Router } from '@angular/router';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
@@ -18,8 +17,6 @@ import { UtilService } from 'src/app/services/util/util.service';
 })
 export class BuzonComponent implements OnInit {
 
-
-
   public estadoActual = 2
   public estatusAcutal = 1
 
@@ -31,7 +28,6 @@ export class BuzonComponent implements OnInit {
   public vprocesados = false
 
   public vpendientes = false
-
 
   public cmbDestino = 'S'
 
@@ -54,27 +50,7 @@ export class BuzonComponent implements OnInit {
   public xAPI: IAPICore = {
     funcion: '',
     parametros: '',
-    relacional: false,
-    concurrencia: false,
-    protocolo: '',
-    ruta: '',
-    version: '',
-    retorna: false,
-    migrar: false,
-    http: 0,
-    https: 0,
-    consumidores: '',
-    puertohttp: 0,
-    puertohttps: 0,
-    driver: '',
-    query: '',
-    metodo: '',
-    tipo: '',
-    prioridad: '',
-    entorno: '',
-    logs: false,
-    cache: 0,
-    estatus: false
+    valores : ''
   }
   lst = []
   public lstEstados = [] //Listar Estados
@@ -83,11 +59,8 @@ export class BuzonComponent implements OnInit {
 
   selNav = 0
 
-  public bzRecibido = []
-  public bzRecibido0 = []
-  public bzProcesados = []
-  public bzPendientes = []
-  public bzCerrados = []
+  public bzOriginal = [] //Listado Original
+  public buzon = []
 
 
   public bzBusqueda = []
@@ -97,18 +70,14 @@ export class BuzonComponent implements OnInit {
 
   public estilocheck = 'none'
   public estiloclasificar = 'none'
-  public allComplete: boolean = false
   public numControl = ''
   public Observacion = ''
   public AccionTexto: string = '0'
   public extender_plazo: any
 
-
-
-
   longitud = 0;
   pageSize = 10;
-  pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageSizeOptions: number[] = [5, 10, 25, 50, 100];
 
 
   // MatPaginator Output
@@ -130,58 +99,19 @@ export class BuzonComponent implements OnInit {
 
   constructor(
     private apiService: ApiService,
-    config: NgbModalConfig,
     private ruta: Router,
     private toastrService: ToastrService,
     private utilService: UtilService,
     private loginService: LoginService,
     private modalService: NgbModal) {
-    // customize default values of modals used by this component tree
-    config.backdrop = 'static';
-    config.keyboard = false;
+      
 
   }
 
   ngOnInit(): void {
     this.listarEstados()
     this.seleccionNavegacion(0)
-
-    this.ConsultarAlertas()
-
   }
-
-
-  // seleccionLista(event) {
-  //   this.longitud = 0;
-  //   this.pageSize = 10;
-  //   const patron = new RegExp(this.utilService.ConvertirCadena(this.buscar))
-  //   if (event.charCode == 13) {
-  //     this.longitud = this.bzBusqueda.length
-  //     if (this.posicionPagina == 3) {
-  //       this.bzBusqueda = this.bzAlertasO.filter((e) => {
-  //         return patron.test(this.utilService.ConvertirCadena(e.busqueda))
-  //       })
-  //       this.bzAlertas = this.bzBusqueda.slice(0, this.pageSize)
-  //     }
-  //     this.buscar = ''
-  //   }
-  // }
-  seleccionLista(event) {
-    this.longitud = 0;
-
-    if (event.charCode == 13) {
-      const patron = new RegExp(this.utilService.ConvertirCadena(this.buscar))
-      this.longitud = this.bzBusqueda.length
-      this.bzRecibido = this.bzAlertasO.filter((e) => { return patron.test(e.busqueda) })
-      this.bzAlertasO = this.bzRecibido.slice(0, this.pageSize)
-    }
-
-  }
-
-
-
-
-
 
   async ConsultarAlertas() {
     this.xAPI.funcion = 'WKF_CAlertas'
@@ -217,38 +147,29 @@ export class BuzonComponent implements OnInit {
 
 
   seleccionNavegacion(e) {
-    this.bzRecibido = []
-    this.bzProcesados = []
-    this.bzPendientes = []
-    this.bzCerrados = []
+    this.buzon = []
     this.xAPI.funcion = 'WKF_CDocumentos'
     this.xAPI.valores = ''
     this.selNav = e
 
+    this.clasificacion = false
+    this.cargarAcciones(e)
+
     switch (e) {
       case 0:
-        this.cargarAcciones(0)
-        this.clasificacion = false
-
         this.xAPI.parametros = this.estadoActual + ',' + this.estatusAcutal
-        this.listarBuzon(this.bzRecibido)
+        this.listarBuzon()
         break
       case 1:
-        this.cargarAcciones(1)
-        this.clasificacion = false
         this.xAPI.parametros = this.estadoActual + ',' + 2
-        this.listarBuzon(this.bzProcesados)
+        this.listarBuzon()
         break
       case 2:
-        this.cargarAcciones(2)
-        this.clasificacion = false
         this.xAPI.parametros = this.estadoActual + ',' + 3
-        this.listarBuzon(this.bzPendientes)
+        this.listarBuzon()
         break
-      case 4:
-        this.xAPI.parametros = this.estadoActual + ',' + 4
-        this.listarBuzon(this.bzCerrados)
-        break
+      case 3:
+        this.ConsultarAlertas()
       default:
         break
     }
@@ -271,10 +192,11 @@ export class BuzonComponent implements OnInit {
     )
   }
 
-  async listarBuzon(bz: any) {
+  async listarBuzon() {
+    var bz = []
+
     await this.apiService.Ejecutar(this.xAPI).subscribe(
       (data) => {
-
         data.Cuerpo.forEach(e => {
           e.existe = e.anom == '' ? true : false
           e.privado = e.priv == 1 ? true : false
@@ -284,11 +206,10 @@ export class BuzonComponent implements OnInit {
           bz.push(e)
         })//Registros recorridos como elementos
 
-        //this.longitud = data.Cuerpo.length
-        this.longitud = this.bzRecibido.length
+        this.longitud = bz.length
         if (this.longitud > 0) {
           this.estilocheck = ''
-          this.bzRecibido0=this.bzRecibido
+          this.bzOriginal = bz
           this.recorrerElementos(0)
         }
 
@@ -299,55 +220,19 @@ export class BuzonComponent implements OnInit {
     )
   }
 
-
-  // pageChangeEvent(e) {
-  //   this.pageSize = e.pageSize
-  //   this.recorrerElementos(e.pageIndex +1, this.lst)
-  // }
   pageChangeEvent(e) {
     this.pageSize = e.pageSize
     this.recorrerElementos(e.pageIndex)
   }
 
-  updateAllComplete() {
-    this.allComplete = this.bzRecibido != null && this.bzRecibido.every(t => t.completed);
-  }
-
-  setAll(completed: boolean) {
-    this.allComplete = completed;
-    if (this.bzRecibido == null) {
-      return;
-    }
-
-    this.bzRecibido.forEach(t => (t.completed = completed));
-    if (completed == false) {
-      this.estiloclasificar = 'none'
-    } else {
-      this.estiloclasificar = ''
-    }
-  }
-
-  someComplete(): boolean {
-    if (this.bzRecibido == null) return false;
-    return this.bzRecibido.filter(t => t.completed).length > 0 && !this.allComplete;
-  }
 
 
   //recorrerElementos para paginar listados
   recorrerElementos(pagina: number) {
     let pag = this.pageSize * pagina
-    console.log(pag)
-    console.log(this.pageSize)
-    this.bzRecibido = this.bzRecibido0.slice(pag, pag + this.pageSize)
-   
-  }
-  // }
-  // recorrerElementos(pagina: number, lista: any) {
-  //   let pag = this.pageSize * pagina
-  //   this.lst = lista.slice(pag, pag + this.pageSize)
-  // }
+    this.buzon = this.bzOriginal.slice(pag, pag + this.pageSize)
 
-  
+  }
 
 
   //editar
