87835332a14fbadba8616eae7820f16c037bad34
Carlos Peña <gesaodin@gmail.com>
2022-06-27 13:22:38 -0400

    Resoluciones Busqueda

--- src/app/views/resoluciones/resoluciones.component.ts
+++ src/app/views/resoluciones/resoluciones.component.ts
@@ -12,31 +12,31 @@ import { LoginService } from 'src/app/services/seguridad/login.service';
 })
 export class ResolucionesComponent implements OnInit {
 
-  public focus : boolean = true
-  public buscar : string = ''
+  public focus: boolean = true
+  public buscar: string = ''
   lst = []
   lengthOfi = 0;
   pageSizeOfi = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
 
-  public xApi : IAPICore = {
+  public xApi: IAPICore = {
     funcion: '',
     parametros: ''
-  } 
+  }
 
   public SubMenu = []
 
-  constructor(private apiService : ApiService, 
-    private resolucionService : ResolucionService,
+  constructor(private apiService: ApiService,
+    private resolucionService: ResolucionService,
     public dialog: MatDialog,
-    public loginService : LoginService,
-    public ruta : Router) { }
+    public loginService: LoginService,
+    public ruta: Router) { }
 
-  async  ngOnInit() {
+  async ngOnInit() {
     await this.loginService.Iniciar()
     this.SubMenu = await this.loginService.obtenerSubMenu(this.ruta.url)
-    if  (sessionStorage.getItem("MPPD_CTipoEntrada") == undefined) {
-      for(var i=0; i < 9; i++){
+    if (sessionStorage.getItem("MPPD_CTipoEntrada") == undefined) {
+      for (var i = 0; i < 9; i++) {
 
         this.resolucionService.Listar(i) //
       }
@@ -44,22 +44,19 @@ export class ResolucionesComponent implements OnInit {
   }
 
 
-  CMppd(){
-    this.xApi.funcion = "C_Mppd"
-    this.xApi.parametros = this.buscar
-    
-    this.apiService.Ejecutar(this.xApi).subscribe(
-      (data) => {
-        this.lst = data.Cuerpo
-      },
-      (error) => {
-        console.error("Error de conexion a los datos ", error)
-      }
-
-    )
-
-    
-   
+  Consultar(event) {
+    if (event.charCode == 13) {
+      this.xApi.funcion = "MPPD_CGeneral"
+      this.xApi.parametros = this.buscar
+      this.apiService.Ejecutar(this.xApi).subscribe(
+        (data) => {
+          this.lst = data.Cuerpo
+        },
+        (error) => {
+          console.error("Error de conexion a los datos ", error)
+        }
+      )
+    }
   }
 
 }
