c283df671f27633af71e61168b6b00a6748cf25f
Carlos Peña <gesaodin@gmail.com>
2024-07-05 09:58:19 -0400

    Funcion de Generales

--- src/app/views/resoluciones/rsconfiguracion/rsgenerales/rsgenerales.component.ts
+++ src/app/views/resoluciones/rsconfiguracion/rsgenerales/rsgenerales.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbDateParserFormatter, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ApiService, IAPICore } from 'src/app/services/apicore/api.service';
 import { UtilService } from 'src/app/services/util/util.service';
 
@@ -26,6 +27,7 @@ export class RsgeneralesComponent implements OnInit {
   constructor(private apiService: ApiService,
     private modalService: NgbModal,
     private ruta: Router,
+    private ngxService: NgxUiLoaderService,
     public formatter: NgbDateParserFormatter,
     private rutaActiva: ActivatedRoute,
     private utilService: UtilService) { }
@@ -38,14 +40,17 @@ export class RsgeneralesComponent implements OnInit {
   }
 
   ConsultarListado(){
-    this.xAPI.funcion = ''
+    this.xAPI.funcion = 'MPPD_CLibroGenerales'
     this.xAPI.parametros = ''
     this.xAPI.valores = ''
-
+    this.ngxService.startLoader("loader-gennerales");
     this.apiService.Ejecutar(this.xAPI).subscribe(
       (data) => {
        
         console.log(data)
+        
+        this.lstGenerales = data.Cuerpo.length > 0? data.Cuerpo: []
+        this.ngxService.stopLoader("loader-gennerales");
 
       },
       (error) => {
