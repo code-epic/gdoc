d3c4dfc07aa520c77f83676ad0bbd066a23467e9
johamguilarte <johamgregorio@gmail.com>
2024-08-05 14:54:49 -0400

    table-grallibro-modal.component.ts mostras 4 x 6 columas y filas

--- src/app/views/generales/grallibro/modal/table-grallibro-modal/table-grallibro-modal.component.ts
+++ src/app/views/generales/grallibro/modal/table-grallibro-modal/table-grallibro-modal.component.ts
@@ -49,6 +49,7 @@ export class TableGrallibroModalComponent implements OnInit {
 
     }
 
+    //ESTE METODO ES PARA MOSTRAR 4 X 6
     convertObject() {
         const listFourGenerales: any[] = [];
         const listSixGenerales: any[] = [];
@@ -60,11 +61,11 @@ export class TableGrallibroModalComponent implements OnInit {
         this.lstGenerales.forEach((element, index) => {
             items.push({...element, index: (index + 1)});
 
-            if (items.length === 6) {
+            if (items.length === 8) {
                 listFourGenerales.push(...items);
                 items.length = 0;
             } else if (listFourGenerales.length > 1) {
-                if (items.length === 4) {
+                if (items.length === 6) {
                     listSixGenerales.push(...items);
                     items.length = 0;
                 } else if ((this.lstGenerales.length - 1) === index) {
@@ -73,29 +74,78 @@ export class TableGrallibroModalComponent implements OnInit {
                 }
             }
         });
-        const first = listFourGenerales.slice(0, 3);
-        const second = listFourGenerales.slice(3, listFourGenerales.length);
+        const first = listFourGenerales.slice(0, 4);
+        const second = listFourGenerales.slice(4, listFourGenerales.length);
 
         this.firstElements = [];
         this.firstElements.push({column1: first, column2: second});
 
         const element = [];
-        for (let i = 0; i < listSixGenerales.length; i += 4) {
+        for (let i = 0; i < listSixGenerales.length; i += 6) {
 
-            const iterator = listSixGenerales.slice(i, i + 4);
+            const iterator = listSixGenerales.slice(i, i + 6);
             element.push([...iterator]);
             if (element.length === 2) {
                 this.secondElements.push({
                     column1: element[0], column2: element[1]
                 });
                 element.length = 0;
-            } else if (iterator.length < 4) {
+            } else if (iterator.length < 6) {
                 this.secondElements.push({
                     column1: element[0]
                 });
             }
         }
     }
+
+    // ESTE METODO ES PARA MOSTRAR 3 X 4
+    // convertObject() {
+    //     const listFourGenerales: any[] = [];
+    //     const listSixGenerales: any[] = [];
+    //
+    //     const items = [];
+    //     this.firstElements = [];
+    //     this.secondElements = [];
+    //     // tslint:disable-next-line:no-shadowed-variable
+    //     this.lstGenerales.forEach((element, index) => {
+    //         items.push({...element, index: (index + 1)});
+    //
+    //         if (items.length === 6) {
+    //             listFourGenerales.push(...items);
+    //             items.length = 0;
+    //         } else if (listFourGenerales.length > 1) {
+    //             if (items.length === 4) {
+    //                 listSixGenerales.push(...items);
+    //                 items.length = 0;
+    //             } else if ((this.lstGenerales.length - 1) === index) {
+    //                 listSixGenerales.push(...items);
+    //                 items.length = 0;
+    //             }
+    //         }
+    //     });
+    //     const first = listFourGenerales.slice(0, 3);
+    //     const second = listFourGenerales.slice(3, listFourGenerales.length);
+    //
+    //     this.firstElements = [];
+    //     this.firstElements.push({column1: first, column2: second});
+    //
+    //     const element = [];
+    //     for (let i = 0; i < listSixGenerales.length; i += 4) {
+    //
+    //         const iterator = listSixGenerales.slice(i, i + 4);
+    //         element.push([...iterator]);
+    //         if (element.length === 2) {
+    //             this.secondElements.push({
+    //                 column1: element[0], column2: element[1]
+    //             });
+    //             element.length = 0;
+    //         } else if (iterator.length < 4) {
+    //             this.secondElements.push({
+    //                 column1: element[0]
+    //             });
+    //         }
+    //     }
+    // }
 
     printPage() {
         const printContents = document.getElementById('print')?.innerHTML;
