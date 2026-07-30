afe18f9d4553a2d57b574d4efedd679f6bf2504f
Carlos E. Peña A. <gesaodin@gmail.com>
2026-07-30 09:34:58 -0400

    Verificacion de errores VI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GDoc MPPD';
+  updateAvailable = false;
 
-  constructor() { }
+  constructor(private swUpdate: SwUpdate) { }
 
   ngOnInit() {
-  
+    // Si las notificaciones de Service Worker están habilitadas
+    if (this.swUpdate.isEnabled) {
+      // Nos suscribimos para saber si hay una actualización lista
+      this.swUpdate.available.subscribe(event => {
+        console.log('Nueva actualización disponible:', event);
+        this.updateAvailable = true;
+      });
+
+      // Forzar una revisión al iniciar la aplicación
+      this.swUpdate.checkForUpdate().then(() => {
+        console.log('Revisión de actualización completada.');
+      }).catch(err => {
+        console.error('Error revisando actualizaciones:', err);
+      });
+    }
   }
 
-  
+  reloadPage() {
+    this.swUpdate.activateUpdate().then(() => {
+      document.location.reload();
+    });
+  }
 }
